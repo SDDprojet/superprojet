0964d45cf1f33db27a19fabd3de3067547e2a455
Munsup Warintara <cartooneiei55@hotmail.com>
2023-04-12 16:15:37 +0200

    panda

--- ref.c
+++ ref.c
@@ -14,14 +14,59 @@
 #include <dirent.h>
 #define SIZE 20
 
-void initRefs():
+void initRefs(){ // à tester 
+    DIR* dir =opendir(".refs");
+    if(dir == NULL){
+        system("mkdir .refs");
+        system("touch .refs/master");
+        system("touch .refs/HEAD");
+        return;
+    }
+    system("touch .refs/master");
+    system("touch .refs/HEAD");
+    closedir(dir);
+}
+
+void createUpdateRef(char* ref_name, char* hash): //à tester
+    if(ref_name == NULL || hash == NULL){
+        return;
+    }
+    if(file_exists(ref_name) == 0){
+        system("mkdir %s",ref_name);
+        system(" echo %s > %s",ref_name,hash);
+    }
+    system(" echo %s > %s",ref_name,hash);
+
+void deleteRef(char* ref_name){ //à tester
+    if(ref_name == NULL){
+        return ;
+    }
+    if(file_exists(ref_name) == 0){
+        printf("fichier n'existe pas, il n'y a pas à supprimer! \n");
+        return ;
+    }
+    system("rm .ref/%s",ref_name);
     return;
 
-void createUpdateRef(char* ref name, char* hash):
-    return;
+}
+char* getRef(char* ref_name){
+    if(ref_name == NULL){
+        return ;
+    }
+    if(file_exists(ref_name) == 0){
+        printf("fichier n'existe pas \n");
+        return ;
+    }
+    FILE* f = fopen(ref_name,"r");
+    if(f == NULL){
+        printf("erreur d'ouverture de ficher \n");
+        return;
+    }
+    char[100] ligne;
+    fgets(ligne,100,f) ;
+    fclose (f) ;
 
-void deleteRef(char* ref name):
-    return;
+    return ligne;
+}
 
-char* getRef(char* ref name):
-    return;
+    
