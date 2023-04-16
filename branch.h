a3589ec7f81b0a1ce762a4a5d318a55a751c6aad
massyl12 <benganamassyl56@gmail.com>
2023-04-16 23:51:36 +0200

    hfdm

--- branch.h
+++ branch.h
@@ -1,5 +1,12 @@
-#ifndef branch_H
-#define branch_H
+#ifndef BRANCH_H
+#define BRANCH_H
+
+typedef struct cell{
+char* data ;
+struct cell* next ;
+}Cell ;
+
+typedef Cell* List ;
 
 void initBranch();
 
