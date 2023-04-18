8ee214c530de53886e0728857a2fb20ca0d19a5b
massyl12 <benganamassyl56@gmail.com>
2023-04-18 23:20:49 +0200

    ghghgh

--- ref.c
+++ ref.c
@@ -20,21 +20,22 @@
 
 
 void initRefs(){ //deja tester
+//initialise une ref
     DIR* dir = opendir(".refs");
     if(dir == NULL){
         int ret = system("mkdir .refs");
         if(ret == -1){
-            printf("Erreur lors de la création du répertoire .refs\n");
+            printf("Erreur lors de la création du répertoire .refs : initref\n");
             return;
         }
         ret = system("touch .refs/master");
         if(ret == -1){
-            printf("Erreur lors de la création du fichier .refs/master\n");
+            printf("Erreur lors de la création du fichier .refs/master : initref\n");
             return;
         }
         ret = system("touch .refs/HEAD");
         if(ret == -1){
-            printf("Erreur lors de la création du fichier .refs/HEAD\n");
+            printf("Erreur lors de la création du fichier .refs/HEAD:initref\n");
             return;
         }
         return;
@@ -44,12 +45,12 @@ void initRefs(){ //deja tester
     closedir(dir);
 }
 
-void createUpdateRef( char *ref_name, char *hash)
-{
+void createUpdateRef( char *ref_name, char *hash){//deja testé
+//cree ou met a jour une ref
   char path[256] = ".refs/";
 
   if(ref_name == NULL){
-    printf( "Chaine de charactère null...");
+    printf( "Chaine de charactère null...: createUpdateRef");
     return;
   }
 
@@ -57,7 +58,7 @@ void createUpdateRef( char *ref_name, char *hash)
   FILE *f = fopen(path, "w");
 
   if(f == NULL){
-    printf("Problème lors de l'ouverture du fichier %s", path);
+    printf("Problème lors de l'ouverture du fichier : createUpdateref%s", path);
   }
 
   fputs(hash, f);
@@ -65,6 +66,7 @@ void createUpdateRef( char *ref_name, char *hash)
   fclose(f);
 }
 void deleteRef(char* ref_name){ //déjà testé 
+//suprime une ref
     if(ref_name == NULL){
         return;
     }
@@ -83,15 +85,15 @@ void deleteRef(char* ref_name){ //déjà testé
     }
 }
 char* getRef(char* ref_name){
-	//printf("ENTER getRef\n");
+//retourne la ref 
 
 	// Permet de rentrer dans le repertoire .refs
-	char buff_path_to_ref[255];
-	sprintf(buff_path_to_ref,".refs/%s",ref_name);
-	FILE* f = fopen(buff_path_to_ref,"r");
-	//printf("PATH =%s\n",buff_path_to_ref);
+	char buff[255];
+	sprintf(buff,".refs/%s",ref_name);
+	FILE* f = fopen(buff,"r");
+
 	if(f == NULL){
-		//printf("PAS DE BOL\n");
+    printf("Erreur lors de l'ouverture du fichier .refs/ : getref %s\n",ref_name);
 		return NULL;
 	}
 	char *buff_hash = (char*)malloc(sizeof(char)*255);
@@ -99,16 +101,14 @@ char* getRef(char* ref_name){
 		char* buff_vide = malloc(sizeof(char)*2);
 		sprintf(buff_vide," ");
 		fclose(f);
-		//printf("àà:%s:\n",buff_vide);
 		return buff_vide;
 	}
-	//printf("%s",buff_hash);
 	fclose(f);
-	//printf("àà:%s\n",buff_hash);
 	return buff_hash;
 }
 
 void createFile(char* file) { //déjà testé
+//cree un fichier 
     if(file == NULL) {
         printf("Le nom de fichier est NULL!\n");
         return;
@@ -121,6 +121,7 @@ void createFile(char* file) { //déjà testé
 }
  
 void myGitAdd(char* file_or_folder) {
+//ajoute un fichier ou un repertoire dans une zone de preparation
     WorkTree* wt = NULL;
     if(file_exists(".add") == 0) {
         createFile(".add");
@@ -140,8 +141,8 @@ void myGitAdd(char* file_or_folder) {
     free(wt);
 }
 
-void myGitCommit(char *branch_name, char *message)
-{
+void myGitCommit(char *branch_name, char *message){
+  //cree un commit dans branch_name avec le message
   if(!file_exists(".refs")){
     printf("Il faut d'abord initialiser les références du projets");
     return;
@@ -154,14 +155,14 @@ void myGitCommit(char *branch_name, char *message)
     return;
   }
 
-  char *last_hash = getRef(branch_name);
+  char *predecessor = getRef(branch_name);
   char *head_hash = getRef("HEAD");
 
-  bool is_different = strcmp(last_hash, head_hash);
+  bool est_different = strcmp(predecessor, head_hash);
 
   free(head_hash);
 
-  if(is_different){
+  if(est_different){
     printf("HEAD doit pointer sur le dernier commit de la branche %s", branch_name);
     return;
   }
@@ -177,13 +178,13 @@ void myGitCommit(char *branch_name, char *message)
   Commit *c = createCommit(hash);
   free(hash);
 
-  if (strlen(last_hash) > 0){
-    commitSet(c, "predecessor", last_hash);
+  if (strlen(predecessor) > 0){
+    commitSet(c, "predecessor", predecessor);
   }
   if(message != NULL){
-    commitSet(c, "message", message); // commit["message"] = message;
+    commitSet(c, "message", message); 
   }
-  free(last_hash);
+  free(predecessor);
 
   char *commit_hash = blobCommit(c);
   createUpdateRef(branch_name, commit_hash);
